e7233fb8b13f75a2c3d17c9f851cab704b10adc5
Максим Сухарев <sucharev_m@hotmail.com>
2023-10-21 00:38:04 +0300

    Создание промежуточного класса для сериализованных объектов в XML

---
+++ CurencyRateConsoleApp/CurrencyXML.cs
@@ -0,0 +1,39 @@
+﻿using System.Xml.Serialization;
+
+public class CurrencyXML
+{
+    [XmlAttribute("ID")]
+    public string ID { get; set; }
+
+    [XmlElement("NumCode")]
+    public string NumCode { get; set; }
+
+    [XmlElement("CharCode")]
+    public string CharCode { get; set; }
+
+    [XmlElement("Nominal")]
+    public int Nominal { get; set; }
+
+    [XmlElement("Name")]
+    public string Name { get; set; }
+
+    [XmlElement("Value")]
+    public string Value { get; set; }
+
+
+    [XmlElement("VunitRate")]
+    public string VunitRate { get; set; }
+
+    public CurrencyXML() { }
+
+    public CurrencyXML(string id, string numCode, string charCode, int nominal, string name, string value, string vUnitRate)
+    {
+        ID = id;
+        NumCode = numCode;
+        CharCode = charCode;
+        Nominal = nominal;
+        Name = name;
+        Value = value;
+        VunitRate = vUnitRate;
+    }
+}
