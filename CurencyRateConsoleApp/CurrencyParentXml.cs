5b3c5aaf1358a4e959bedc1709523ea6b77ec817
Максим Сухарев <sucharev_m@hotmail.com>
2023-10-21 00:44:19 +0300

    Создание промежуточного родительского класса для сериализованного XML

---
+++ CurencyRateConsoleApp/CurrencyParentXml.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Serialization;
+
+[Serializable]
+public class CurrencyParentXml
+{
+    [XmlAttribute("Date")]
+    public string Date { get; set; }
+
+    [XmlAttribute("name")]
+    public string Name { get; set; }
+
+    [XmlElement("Valute")]
+    public List<CurrencyXML> CurrencyXMLs { get; set; }
+}
