1993098a0e45cb66738fb7818441f22064710c24
Максим Сухарев <sucharev_m@hotmail.com>
2023-10-20 23:47:17 +0300

    Создание моделей данных

---
+++ CurencyRateConsoleApp/Currency.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Currency
+{
+    public string Code { get; set; }
+    public string Name { get; set; }
+
+    public virtual ICollection<CurrencyRate> Rate { get; set; }
+}
---
+++ CurencyRateConsoleApp/CurrencyRate.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class CurrencyRate
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public string CurrencyCode { get; set; }
+    public double Value { get; set; }
+
+    public virtual Currency Currency { get; set; }
+}
