95bae43f6c1bd13f885b433db73039255893f2f8
Максим Сухарев <sucharev_m@hotmail.com>
2023-10-21 01:29:00 +0300

    Добавление сервиса курса валют + метод получения ответа

---
+++ CurencyRateConsoleApp/CurrencyService.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.SqlTypes;
+using System.Globalization;
+using System.Xml;
+using System.Xml.Linq;
+
+public class CurrencyService
+{
+    private readonly HttpClient _httpClient;
+
+    public CurrencyService()
+    {
+        _httpClient = new HttpClient();
+    }
+
+    public async Task<string> GetCurrencyRates(DateTime date)
+    {
+        var url = $"https://cbr.ru/scripts/XML_daily.asp?date_req={date.Day}/{date.Month}/{date.Year}";
+               
+        var response = await _httpClient.GetAsync(url);
+
+        string responseXml = string.Empty;
+
+        if (response.IsSuccessStatusCode) responseXml = await response.Content.ReadAsStringAsync();
+        else Console.WriteLine($"Ошибка в запросе. Код ошибки: {response.StatusCode}");
+
+        return responseXml;
+    }
+
+
+}
