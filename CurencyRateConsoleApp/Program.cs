e3cb4105380f1152df4fb40fcb7ca1173f6b3f72
Oswitto624 <dzirtsmd@mail.ru>
2023-10-21 16:47:16 +0300

    Рефакторинг кода

--- CurencyRateConsoleApp/Program.cs
+++ CurencyRateConsoleApp/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-
+#region Попытка подключения БД v1
 //var services = new ServiceCollection();
 
 //var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
@@ -19,32 +19,36 @@ using Microsoft.Extensions.DependencyInjection;
 
 //var serviceProvider = services.BuildServiceProvider();
 //var dbContext = serviceProvider.GetRequiredService<DbContext>();
+#endregion
 
-using (CurrencyDB context = new CurrencyDB())
-{
-    context.Database.EnsureDeleted();
-    context.Database.EnsureCreated();
+#region Костыльное создание БД, используя тестовые данные v2
+//using (CurrencyDB context = new CurrencyDB())
+//{
+//    context.Database.EnsureDeleted();
+//    context.Database.EnsureCreated();
 
 
-    var newCurrency = new Currency
-    {
-        CharCode = "ABC",
-        Name = "test",
-        Id_cbr = "ABC1",
-        NumCode = "1234",
-        Nominal = 1234,
-    };
+//    var newCurrency = new Currency
+//    {
+//        CharCode = "ABC",
+//        Name = "test",
+//        Id_cbr = "ABC1",
+//        NumCode = "1234",
+//        Nominal = 1234,
+//    };
 
-    var newCurrencyRate = new CurrencyRate
-    {
-        Currency = newCurrency,
-        Date = new DateTime(),
-        Value = 0.1,        
-    };
+//    var newCurrencyRate = new CurrencyRate
+//    {
+//        Currency = newCurrency,
+//        Date = new DateTime(),
+//        Value = 0.1,        
+//    };
 
-    context.Currencies.Add(newCurrency);
-    context.SaveChanges();
-    context.CurrencyRates.Add(newCurrencyRate);
-    context.SaveChanges();
-}
+//    context.Currencies.Add(newCurrency);
+//    context.SaveChanges();
+//    context.CurrencyRates.Add(newCurrencyRate);
+//    context.SaveChanges();
+//}
+#endregion
+
 
