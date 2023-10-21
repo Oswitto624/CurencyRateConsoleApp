19a3b7ad1d1bcaf88c3e03dfad1cecd46f1e0cc4
Oswitto624 <dzirtsmd@mail.ru>
2023-10-21 16:44:15 +0300

    Создание контекста БД, создание миграции (пришлось заполнять тестовыми данными, т.к. через консоль EF Core никак не получалось)

--- CurencyRateConsoleApp/Program.cs
+++ CurencyRateConsoleApp/Program.cs
@@ -1,3 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DAL.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
+
+//var services = new ServiceCollection();
+
+//var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+//    .AddJsonFile("appsettings.json")
+//    .Build();
+
+//var connectionString = configuration.GetConnectionString("SqlServer");
+
+//services.AddDbContext<CurrencyDB>(opt =>
+//{
+//    opt.UseSqlServer(connectionString);
+//});
+
+
+//var serviceProvider = services.BuildServiceProvider();
+
+using (CurrencyDB context = new CurrencyDB())
+{
+    context.Database.EnsureDeleted();
+    context.Database.EnsureCreated();
+
+
+    var newCurrency = new Currency
+    {
+        CharCode = "ABC",
+        Name = "test",
+        Id_cbr = "ABC1",
+        NumCode = "1234",
+        Nominal = 1234,
+    };
+
+    var newCurrencyRate = new CurrencyRate
+    {
+        Currency = newCurrency,
+        Date = new DateTime(),
+        Value = 0.1,        
+    };
+
+    context.Currencies.Add(newCurrency);
+    context.SaveChanges();
+    context.CurrencyRates.Add(newCurrencyRate);
+    context.SaveChanges();
+
+    //context.Dispose();
+
+}
+
+//var dbContext = serviceProvider.GetRequiredService<DbContext>();
--- DAL/Context/CurrencyDB.cs
+++ DAL/Context/CurrencyDB.cs
@@ -1,13 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
 
 namespace DAL.Context;
-
 
 public class CurrencyDB : DbContext
 {
     public DbSet<Currency> Currencies { get; set; }
-
     public DbSet<CurrencyRate> CurrencyRates { get; set; }
 
-    public CurrencyDB(DbContextOptions<CurrencyDB> options) : base(options) { }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CurrencyDB;");
+    }
+
+    //public CurrencyDB() { }
+    //public CurrencyDB(DbContextOptions<CurrencyDB> options) : base(options) { }
+
 }
