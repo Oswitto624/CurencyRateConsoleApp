7e77e17941fb8fe008144fdeebb28730896a9337
Максим Сухарев <sucharev_m@hotmail.com>
2023-10-21 03:06:03 +0300

    Создание DAL. Установление зависимостей, установка Nuget-пакетов EF

---
+++ DAL/Context/CurrencyDB.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Context;
+
+
+public class CurrencyDB : DbContext
+{
+    public DbSet<Currency> Currencies { get; set; }
+
+    public DbSet<CurrencyRate> CurrencyRates { get; set; }
+
+    public CurrencyDB(DbContextOptions<CurrencyDB> options) : base(options) { }
+}
